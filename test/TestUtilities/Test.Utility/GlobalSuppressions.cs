78a40d5a67d40009ffe597f324ff3ac53194e129
Nikolche Kolev <nikolev@microsoft.com>
2021-10-01 12:13:06 -0700

    Onboard API banning

---
+++ test/TestUtilities/Test.Utility/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("ApiDesign", "RS0030:Do not used banned APIs", Justification = "This is a test helper utility.", Scope = "member", Target = "~M:Test.Utility.DebuggerUtils.WaitForDebugger")]
